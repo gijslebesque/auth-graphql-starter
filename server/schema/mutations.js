793a115ce4cdee4829c61868387cd2eaa9bd9c54
gijslebesque <gijslebesque@gmail.com>
2019-09-20 15:12:04 +0200

    signup

---
+++ server/schema/mutations.js
@@ -0,0 +1,35 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const UserType = require('./types/UserType');
+const AuthService = require('../services/auth');
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.signup({ email, password, req });
+      }
+    }
+    // login: {
+    //   type: UserType,
+    //   args: {
+    //     email: { type: GraphQLString },
+    //     password: { type: GraphQLString },
+    //
+    //   },
+    // resolve(parentValue, { email, password }, req) {
+    //       return AuthService.login({ email, password, req });
+    //     }
+    // }
+  }
+});
+
+module.exports = mutation;
